5c5c7cfe3533de7953e988ae76fda00208dc7d9c
bkarim27 <63055727+bkarim27@users.noreply.github.com>
2022-01-27 17:04:04 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,18 @@
 def buildJar() {
     echo "building jar"
-    sh 'ls $WORKSPACE'
     sh 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image"
-    sh 'docker build -t badshak/demo-app:java-maven-app-2.3 .'
+    sh 'docker build -t 139.59.177.91:8083/java-maven-app:1.5 .'
 } 
 
 def deployApp() {
-    echo "deploying to docker hub"
-    withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push badshak/demo-app:java-maven-app-2.3'
+    echo "deploying to nexus"
+    withCredentials([usernamePassword(credentialsId: 'c75c489e-0c72-468e-99fb-0d00eb95766e', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "echo $PASS | docker login -u $USER --password-stdin 139.59.177.91:8083"
+        sh 'docker push 139.59.177.91:8083/java-maven-app:1.5'
     }
 } 
 
